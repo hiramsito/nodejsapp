d9c806756c7a1150ce5cf82ee8c1637130916e5d
Hiram Flores <65650902+hiramsito@users.noreply.github.com>
2022-04-27 18:42:31 -0700

    Update nodejsDockerDSL.groovy

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -14,7 +14,7 @@ job('Practica publish Docker DSL') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('hiramsito/dockerpractica')
+            repositoryName('hiramsito/nodejsapp')
             tag('${GIT_REVISION,length=7}')
             registryCredentials('docker-hub')
             forcePull(false)
